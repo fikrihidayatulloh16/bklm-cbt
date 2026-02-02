e26df82be85680916fd92e1c5cc745eb4a9d728d
fikrihidayatulloh16 <fikrihidayatulloh16@gmail.com>
2026-02-02 23:00:57 +0700

    try ci/cd github action

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: "bklm-api",
+      cwd: "./apps/api",      
+      script: "dist/main.js",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "bklm-web",
+      cwd: "./apps/web",      
+      script: "npm",
+      args: "start",          
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
